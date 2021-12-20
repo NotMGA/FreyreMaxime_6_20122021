66501d8f0cf98505746ff4694a56757ab43d29e7
NotMGA <maximefreyre@gmail.com>
2021-12-20 20:11:09 +0100

    h

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,28 +1,46 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+       fetch('https://raw.githubusercontent.com/NotMGA/Front-End-Fisheye/main/data/photographers.json')    
+       .then(response => {
+            return response.json();
+          }).then(data => {
+            // Work with JSON data here
+            const photographers = data.photographers;
+            
+            return Promise.resolve(photographers)
+            
+          }).catch(err => {
+            // Do something for an error here
+          });
+          // var photographers = data.photographers;
+          // console.log(photographers);
+        // const photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
+        // const photographersjson = [data.photographers];
+        // const photographers = photographersjson[1];
+        // console.log(photographers);
         // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // return ({
+        //     photographers: [...photographers, ...photographers, ...photographers]})
+
     }
 
     async function displayData(photographers) {
