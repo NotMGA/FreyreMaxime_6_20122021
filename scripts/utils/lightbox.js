5416d7baf2d6c5fd36763d98c198dc3772ea6398
NotMGA <maximefreyre@gmail.com>
2022-02-05 23:47:24 +0100

    Version final

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -14,13 +14,12 @@
     const id_photographe = param.get("id");
     console.log("fin load");
     
-    // console.log(lien_media);
+
     for (let media of lien_media){
         media.addEventListener("click", function(e){
             e.preventDefault();
            let video_image = this.href;
            
-        //    console.log(this);
             if( video_image.includes(".mp4") == true){
                 
                 video.src =this.href;
@@ -34,7 +33,6 @@
                 txt_lightbox_video = txt_lightbox_video.replace('.mp4','');
                 txt_lightbox_video = txt_lightbox_video.replace("https://notmga.github.io/FreyreMaxime_6_20122021/assets/photographers/"+id_photographe+"/","");
                 txt_lightbox_video = txt_lightbox_video.replaceAll('_',' ');
-                console.log(txt_lightbox_video);
                 txt_lightbox.innerHTML = txt_lightbox_video;
                
                 
@@ -77,19 +75,15 @@
         document.getElementById("lightbox_fermer").focus();
         
         
-        // console.log( image.src.replace("http://127.0.0.1:5500/assets/photographers/"+id_photographe+"/",""))
         for (let i =0 ;i< lien_media.length; i++ ){
             const image_reduit = lien_media[i].href.replace("../assets/photographers/"+id_photographe+"/","");
             
-            // console.log (image_reduit)
-            // console.log (image_actuel)
+
 
             if (image_reduit == image_actuel ){
                 i++;
                 const video_image = lien_media[i].href;
-                // console.log()
                 
-                // console.log(video_image.includes(".mp4"))
                 if( video_image.includes(".mp4") == true){
                 
                     video.src =lien_media[i].href;
@@ -100,7 +94,6 @@
                     txt_lightbox_video = txt_lightbox_video.replace('.mp4','');
                     txt_lightbox_video = txt_lightbox_video.replace("https://notmga.github.io/FreyreMaxime_6_20122021/assets/photographers/"+id_photographe+"/","");
                     txt_lightbox_video = txt_lightbox_video.replaceAll('_',' ');
-                    console.log(txt_lightbox_video);
                     txt_lightbox.innerHTML = txt_lightbox_video;
                    
                     
@@ -111,7 +104,6 @@
                     image.style.display="initial";
                     video.style.display ="none";
                 }
-                // console.log(lien_media);
                 if (i==lien_media.length -1)
                 {
                     document.querySelector(".lightbox_suivant").style.display ="none";
@@ -122,14 +114,10 @@
                     document.querySelector(".lightbox_precedent").style.display ="initial";
                 }
 
-                // const photo_suivante = lien_media[i].href.replace("http://127.0.0.1:5500/assets/photographers/"+id_photographe+"/","");
-                // console.log(photo_suivante)
-                // image.src = lien_media[i].href ;
+
             }
         }
-        // let i  = lien_media.findIndex(image => image === this.url)
-        // const image = lightbox.querySelector(".lightbox_media img");
-        // image.src =this.href;
+
 
     }
     prec.addEventListener("click",media_prec);
@@ -140,11 +128,9 @@
         const image_actuel = image.src.replace("../assets/photographers/"+id_photographe+"/","");
         
         
-        // console.log( image.src.replace("http://127.0.0.1:5500/assets/photographers/"+id_photographe+"/",""))
         for (let i =0 ;i< lien_media.length; i++ ){
             const image_reduit = lien_media[i].href.replace("../assets/photographers/"+id_photographe+"/","");
-            // console.log (image_reduit)
-            // console.log (image_actuel)
+
             if (image_reduit == image_actuel ){
                 i-1;
                 const video_image = lien_media[i-1].href;
@@ -159,7 +145,6 @@
                     txt_lightbox_video = txt_lightbox_video.replace('.mp4','');
                     txt_lightbox_video = txt_lightbox_video.replace("https://notmga.github.io/FreyreMaxime_6_20122021/assets/photographers/"+id_photographe+"/","");
                     txt_lightbox_video = txt_lightbox_video.replaceAll('_',' ');
-                    console.log(txt_lightbox_video);
                     txt_lightbox.innerHTML = txt_lightbox_video;
 
                    
@@ -172,7 +157,6 @@
                     video.style.display ="none";
                     txt_lightbox.innerHTML =lien_media[i-1].querySelector("img").alt;
                 }
-                console.log(i)
                 if (i==1)
                 {
                     document.querySelector(".lightbox_precedent").style.display ="none";
@@ -182,15 +166,10 @@
                     document.querySelector(".lightbox_suivant").style.display ="initial";
                     document.querySelector(".lightbox_precedent").style.display ="initial";
                 }
-                // const photo_suivante = lien_media[i].href.replace("http://127.0.0.1:5500/assets/photographers/"+id_photographe+"/","");
-                // console.log(photo_suivante)
-                // console.log(lien_media[i].href);
-                // image.src = lien_media[i-2].href ;
+
             }
         }
-        // let i  = lien_media.findIndex(image => image === this.url)
-        // const image = lightbox.querySelector(".lightbox_media img");
-        // image.src =this.href;
+
 
     }
     window.addEventListener("keyup", function (event) {
@@ -208,12 +187,10 @@
             // Faire quelque chose pour la touche "right arrow" pressée.
             break;
           case "Escape":
-              console.log("ok")
             lightbox.classList.remove("show");
             // Faire quelque chose pour la touche "esc" pressée.
             break;
           default:
-              console.log("def")
             return; // Quitter lorsque cela ne gère pas l'événement touche.
         }
       
