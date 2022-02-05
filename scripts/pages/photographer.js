4e3e233bf54718dadebf4ef41f9276817d3faca1
NotMGA <maximefreyre@gmail.com>
2022-02-05 13:41:13 +0100

    v

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -6,7 +6,7 @@ info_photographe (photographe_id_url);
 // recuperation des donnees .Json
 async function getPhotographers() 
 {
-    let response = await fetch('../data/photographers.json') 
+    let response = await fetch('https://raw.githubusercontent.com/NotMGA/Front-End-Fisheye/main/data/photographers.json') 
    const  photographers = await response.json();
     return photographers; 
 }
